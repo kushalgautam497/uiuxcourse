27a3c13291e5e9b1312c58cac02b9038fe6aafa9
kushal gautam <kushalgautam497@gmail.com>
2026-01-14 12:08:57 +0545

    update cv

---
+++ js/new.js
@@ -0,0 +1,10 @@
+let a = prompt("enter first number")
+let b = prompt("enter first number")
+let c = number (a) + number(b);
+alert (a+"+"+b+"="+c);
+document.writeln ("result is "+c)
+
+
+let arr1 =[];
+let arr2 =[20,36,58,15];
+let arr3 =[];
